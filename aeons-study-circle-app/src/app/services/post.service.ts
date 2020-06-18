227b542184024eb42bc8540e82f4ef5b7a84073c
Sumesh TG <mailbox4sumesh@gmail.com>
2020-06-18 22:33:38 +0530

    added collection constant

--- aeons-study-circle-app/src/app/services/post.service.ts
+++ aeons-study-circle-app/src/app/services/post.service.ts
@@ -6,37 +6,51 @@ import { Post } from 'src/app/models/post.model';
   providedIn: 'root'
 })
 export class PostService {
-
+  POST_COLLECTION_NAME = "posts";
   constructor(private firestore: AngularFirestore) { }
   getPosts() {
-    return this.firestore.collection('posts', ref => ref
+    return this.firestore.collection(this.POST_COLLECTION_NAME, ref => ref
       .limit(5)
       .orderBy('createdDate', 'desc')).snapshotChanges();
   }
   nextPage(lastInResponse) {
-    return this.firestore.collection('posts', ref => ref
+    return this.firestore.collection(this.POST_COLLECTION_NAME, ref => ref
       .limit(5)
       .orderBy('createdDate', 'desc')
       .startAfter(lastInResponse)).snapshotChanges();
   }
   prevPage(firstInResponse, startAt) {
-    return this.firestore.collection('posts', ref => ref
+    return this.firestore.collection(this.POST_COLLECTION_NAME, ref => ref
       .limit(5)
       .orderBy('createdDate', 'desc')
       .startAt(startAt)
       .endBefore(firstInResponse)).snapshotChanges();
   }
   createPost(post: Post) {
-    return this.firestore.collection('posts').add(post);
+    return this.firestore.collection(this.POST_COLLECTION_NAME).add(post);
   }
   updatePost(postModel: Post) {
     delete postModel.id;
-    this.firestore.doc('posts/' + postModel.id).update(postModel);
+    this.firestore.doc(this.POST_COLLECTION_NAME + '/' + postModel.id).update(postModel);
   }
   deletePost(postModel: Post) {
-    this.firestore.doc('posts/' + postModel.id).delete();
+    // return this.firestore.collection('posts').doc(postModel.id).delete().then(function() {
+    //   console.log("Document successfully deleted!");
+    // })
+    // .catch(function(error) {
+    //   console.error("Error removing document: ", error);
+    // });
+    let deleteDoc = this.firestore.collection(this.POST_COLLECTION_NAME, ref => ref.where('id', '==', postModel.id)).snapshotChanges().subscribe(data => {
+      data.map(e => {
+        this.firestore.collection(this.POST_COLLECTION_NAME).doc(e.payload.doc.id).delete().then(function () {
+          console.log("deletedd");
+        }).catch(function (err) {
+          console.log("Error while deleting document", err);
+        });
+      });
+    });
   }
   getPostById(id) {
-    return this.firestore.collection('posts', ref => ref.where('id', '==', id)).snapshotChanges();
+    return this.firestore.collection(this.POST_COLLECTION_NAME, ref => ref.where('id', '==', id)).snapshotChanges();
   }
 }
