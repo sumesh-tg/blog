7778f68732988184136049399972c6eb5da2543d
Sumesh TG <mailbox4sumesh@gmail.com>
2022-01-24 21:49:32 +0530

    Quiz routing
    Quiz routing

--- aeons-study-circle-app/src/app/quiz-dashboard/quiz-dashboard-routing.module.ts
+++ aeons-study-circle-app/src/app/quiz-dashboard/quiz-dashboard-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { QuizComponent } from './components/quiz/quiz.component';
 
 
-const routes: Routes = [{path:"quiz",component:HomeComponent}];
+const routes: Routes = [{path:"quiz",component:HomeComponent,
+children:[
+  {
+    path:'',
+    redirectTo:"/quiz/home",
+    pathMatch:"full"
+  },
+  {
+    path:"home",
+    component:QuizComponent
+  }
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
